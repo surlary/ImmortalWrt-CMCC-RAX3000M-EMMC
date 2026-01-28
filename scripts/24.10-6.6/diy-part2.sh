f723aee861e9fc2b9db53b2752f6f836144abfe6
surlary <xinyy@netease.com>
2026-01-28 16:08:36 +0800

    Update diy-part2.sh

--- scripts/24.10-6.6/diy-part2.sh
+++ scripts/24.10-6.6/diy-part2.sh
@@ -20,12 +20,6 @@ else
   echo "警告：$CONFIG_FILE 不存在，跳过 IP 修改"
 fi
 
-# 预装
-# OpenClash
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
-# 微信推送
-echo "CONFIG_PACKAGE_luci-app-wechatpush=y" >> .config
-echo "CONFIG_PACKAGE_luci-i18n-wechatpush-zh-cn=y" >> .config
 
 sed -i 's/max-frequency = <52000000>;/max-frequency = <26000000>;/g' /workdir/openwrt/target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-emmc.dts
 sed -i "s/if (\$sum ne \$file_hash)/if (\$sum ne \$file_hash \&\& 1==2)/g" /workdir/openwrt/scripts/download.pl
@@ -33,13 +27,6 @@ sed -i "s/if (\$sum ne \$file_hash)/if (\$sum ne \$file_hash \&\& 1==2)/g" /work
 cat /workdir/openwrt/target/linux/mediatek/dts/mt7981b-cmcc-rax3000m-emmc.dts
 cat /workdir/openwrt/scripts/download.pl
 
-# 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
-rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
-if [ $? -eq 0 ]; then
-  echo "已删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p"
-else
-  echo "错误：删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p 失败"
-fi
 
 # 创建 MT7981 固件符号链接
 EEPROM_FILE="package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin"
@@ -53,26 +40,7 @@ else
   exit 1
 fi
 
-echo ">>> 启用 keepalived和kmod-nf-ipvs 模块"
 cat <<EOF >> .config
-CONFIG_PACKAGE_keepalived=y
-CONFIG_PACKAGE_libnl-genl200=y
-CONFIG_PACKAGE_libmagic=y
-CONFIG_PACKAGE_kmod-macvlan=y
-CONFIG_PACKAGE_libnl-route200=y
-CONFIG_PACKAGE_libnfnetlink0=y
-CONFIG_PACKAGE_libip4tc2=y
-CONFIG_PACKAGE_libip6tc2=y
-CONFIG_PACKAGE_libxtables12=y
-CONFIG_PACKAGE_libipset13=y
-CONFIG_PACKAGE_keepalived-sync=y
-CONFIG_PACKAGE_kmod-nf-ipvs=y
-CONFIG_PACKAGE_kmod-ipvs=y
-CONFIG_PACKAGE_kmod-ipvs-core=y
-CONFIG_PACKAGE_kmod-ipvs-rr=y
-CONFIG_PACKAGE_kmod-ipvs-wrr=y
-CONFIG_PACKAGE_kmod-ipvs-sh=y
-
 CONFIG_PACKAGE_aria2=y
 EOF
 
